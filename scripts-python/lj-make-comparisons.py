f1fc1280a9a0f571b35c6321a6d3c72dc66fe778
Jordan-Pommerenck <jordan.pommerenck@gmail.com>
2020-01-24 15:52:28 -0800

    the script now correctly orders the data putting the benchmark data first.

--- scripts-python/lj-make-comparisons.py
+++ scripts-python/lj-make-comparisons.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 import numpy as np
-import matplotlib.pyplot as plt
 import os
+from glob import glob
 import argparse
 
 parser = argparse.ArgumentParser(description =
@@ -15,36 +15,27 @@ parser = argparse.ArgumentParser(description =
 
 parser.add_argument('--N', metavar='cluster_size', type=int, default=1,
                     help='The number of atoms in the LJ cluster -- Default = 1')
-parser.add_argument('--benchmark', required=True,
-                    help='Each file is compared to the benchmark reference -- Required')
-parser.add_argument('--need_wide_cv', action='store_true',
-                    help='A boolean that determines whether we plot a narrow range --Default = False')
+parser.add_argument('--data_dir', required=True,
+                    help='The data directory to search through -- Required \
+                          Example: /home/jordan/sad-monte-carlo/')
+# parser.add_argument('--need_wide_cv', action='store_true',
+#                     help='A boolean that determines whether we plot a narrow range --Default = False')
+
 args = parser.parse_args()
-print(args)
+# Possibly add in (3) more command line arguments that allow us to exactly
+# specify the temperature range to plot.
 
 N=args.N
-need_wide_cv = args.need_wide_cv
-benchmark = args.benchmark # This is bench below!
+#need_wide_cv = args.need_wide_cv
+datadir = args.data_dir
 
-datadir = '../lj-cluster/data/lj%i/' % N
-os.makedirs(datadir, exist_ok=True)
-
-"""
-
-
-    (4) I will put this in lj-cluster/data
-    (5) I will automatically loop through data in given directory
-    (6) I will need four argparse arguments!
-    (7) possibly
-"""
-
-T = np.arange(0.01, 0.05000001, 0.0005)
-wideT = np.arange(0.01, 0.4000001, 0.001)
-
-CV = None
-need_wide_cv = True
-
-def heat_capacity(T, E, S):
+def heat_capacity(T, E, S, Num):
+    """
+        This function calculates the heat capacity by taking in
+        the temperature 'T', energy 'E', and entropy 'S' as NumPy
+        arrays.  The number 'Num' is also necessary to complete
+        the heat capacity calculation.
+    """
     C = np.zeros_like(T)
     for i in range(len(T)):
         boltz_arg = S - E/T[i]
@@ -52,114 +43,94 @@ def heat_capacity(T, E, S):
         P = P/P.sum()
         U = (E*P).sum()
         C[i] = ((E-U)**2*P).sum()/T[i]**2
-    return C + 31*1.5
+    return C + Num*1.5
 
+savedir = '../lj-cluster/data/lj%i/' % N
+os.makedirs(savedir, exist_ok=True)
 
-# old_cvs = []
-#
-# fnames = [
-#     '/home/droundy/src/sad-monte-carlo/tiny-lj-benchmark',
-#     '/home/jordan/sad-monte-carlo/lj-31-benchmark',
-#
-#     '/home/jordan/sad-monte-carlo/lj-sad-31-bin001',
-#     '/home/jordan/sad-monte-carlo/lj-sad-31-bin01',
-#     '/home/jordan/sad-monte-carlo/lj-sad-31-bin002',
-#     # '/home/jordan/sad-monte-carlo/lj-sad-31-bin0005',
-#     '/home/droundy/src/sad-monte-carlo/lj-sad-31-bin0001',
-#
-#     '/home/jordan/sad-monte-carlo/lj-wl-31-bin01',
-#     '/home/jordan/sad-monte-carlo/lj-wl-31-bin001',
-#     '/home/jordan/sad-monte-carlo/lj-wl-31-bin002',
-#     '/home/jordan/sad-monte-carlo/lj-wl-31-bin0005',
-#     '/home/jordan/sad-monte-carlo/lj-wl-31-bin0001',
-#
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin01',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin001',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin001-58',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin001-54',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin001-52',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin002',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin0005',
-#     '/home/jordan/sad-monte-carlo/lj-inv-t-wl-31-bin0001',
-#
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e5-bin01',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e5-bin001',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e5-bin002',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e5-bin0005',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e5-bin0001',
-#
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e6-bin01',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e6-bin001',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e6-bin002',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e6-bin0005',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e6-bin0001',
-#
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e7-bin01',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e7-bin001',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e7-bin002',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e7-bin0005',
-#     '/home/jordan/sad-monte-carlo/lj-samc-31-1e7-bin0001',
-# ]
-#
-# for fname in fnames:
-#     print(fname)
-#     time = np.loadtxt(fname+'.time')
-#     my_energy = np.loadtxt(fname+'.energy')
-#     my_entropy = np.loadtxt(fname+'.entropy')
-#
-#     if CV is None:
-#         Ebest = my_energy;
-#         Sbest = my_entropy[-1,:]
-#         CV = heat_capacity(T, Ebest, Sbest)
-#         np.savetxt(datadir+'bench-cv.txt',
-#                    np.array([T, CV]).transpose(),
-#                    fmt='%.4g', # good enough for our plot
-#         );
-#     elif need_wide_cv:
-#         wideCV = heat_capacity(wideT, my_energy, my_entropy[-1,:])
-#         np.savetxt(datadir+'wide-cv.txt',
-#                    np.array([wideT, wideCV]).transpose(),
-#                    fmt='%.4g', # good enough for our plot
-#         );
-#         need_wide_cv = False
-#
-#     cv_error = []
-#     cv_max_error = []
-#     myt = []
-#     for t in range(len(my_entropy[:, 0])):
-#         if time[t] < 1e7:
-#             continue
-#         myt.append(time[t])
-#         mycv = heat_capacity(T, my_energy, my_entropy[t,:])
-#         plt.clf()
-#         for i in range(len(old_cvs)):
-#             plt.plot(T, old_cvs[i], label=fnames[i])
-#         plt.plot(T, mycv, label=fname+' '+str(time[t]))
-#         plt.legend(loc='best')
-#         plt.xlim(T[0], T[-1])
-#         plt.ylim(80, 130)
-#         plt.pause(0.01)
-#
-#         err = 0
-#         norm = 0
-#         for j in range(1, len(mycv)):
-#             err += abs(CV[j]-mycv[j])
-#             norm += 1.0
-#         cv_error.append(err/norm)
-#         cv_max_error.append(abs(CV-mycv).max())
-#         if time[t] == 1e12:
-#             np.savetxt(datadir+os.path.basename(fname)+'-cv.txt',
-#                        np.array([T, mycv]).transpose(),
-#                        fmt='%.4g', # good enough for our plot
-#             );
-#             np.savetxt(datadir+os.path.basename(fname)+'-wide-cv.txt',
-#                        np.array([wideT, heat_capacity(wideT, my_energy, my_entropy[t,:])]).transpose(),
-#                        fmt='%.4g', # good enough for our plot
-#             );
-#
-#     old_cvs.append(mycv)
-#
-#     np.savetxt(datadir+os.path.basename(fname)+'-cv-error.txt',
-#                np.array([myt, cv_error, cv_max_error]).transpose(),
-#                fmt='%.3g', # low resolution is good enough for error data.
-#     );
+# Define the temperature range
+T = np.arange(0.01, 0.05000001, 0.05000001*0.01)
+wideT = np.arange(0.01, 0.4000001, 0.001)
+
+CV = None
+old_cvs = []
+
+fnames = []
+
+# Find all of the files with the matching number of particles N.
+glob_names = glob((datadir + '*-%i-*' % N))
+if glob_names == []:
+    glob_names = glob((datadir + '*-%i.*' % N))
+
+for glob in glob_names:
+    fnames.append(glob.split('.')[0].replace(datadir,''))
+
+fnames = list(set(fnames.copy()))
+
+# Sort the benchmark files and put them first in the list for comparison.
+bench_files = []
+for i in range(len(fnames)):
+    if 'bench' in fnames[i]:
+        bench_files.append(fnames[i])
+
+bench_files = sorted(bench_files)
+fnames = bench_files + [x for x in fnames if x not in bench_files]
+
+for fname in fnames:
+    print(fname)
+    time = np.loadtxt(datadir+fname+'.time')
+    my_energy = np.loadtxt(datadir+fname+'.energy')
+    my_entropy = np.loadtxt(datadir+fname+'.entropy')
+
+    if CV is None:
+        Ebest = my_energy;
+        Sbest = my_entropy[-1,:]
+        CV = heat_capacity(T, Ebest, Sbest, N)
+        if 'bench' in fname:
+            np.savetxt(savedir +'bench-cv.txt',
+                    np.array([T, CV]).transpose(),
+                    fmt='%.4g', # good enough for our plot
+                    );
+
+    cv_error = []
+    cv_max_error = []
+    myt = []
+
+    for t in range(len(my_entropy[:, 0])):
+        if time[t] < 1e3:
+            continue
+        myt.append(time[t])
+        mycv = heat_capacity(T, my_energy, my_entropy[t,:], N)
+
+        err = 0
+        norm = 0
+        for j in range(1, len(mycv)):
+            err += abs(CV[j]-mycv[j])
+            norm += 1.0
+        cv_error.append(err/norm)
+        cv_max_error.append(abs(CV-mycv).max())
+        # if time[t] == 1e12:
+        #     np.savetxt(datadir+os.path.basename(fname)+'-cv.txt',
+        #                np.array([T, mycv]).transpose(),
+        #                fmt='%.4g', # good enough for our plot
+        #     );
+        #     np.savetxt(datadir+os.path.basename(fname)+'-wide-cv.txt',
+        #                np.array([wideT, heat_capacity(wideT, my_energy, my_entropy[t,:])]).transpose(),
+        #                fmt='%.4g', # good enough for our plot
+        #     );
+
+    #old_cvs.append(mycv)
+    if 'bench' not in fname:
+        np.savetxt(savedir+os.path.basename(fname)+'-cv-error.txt',
+                np.array([myt, cv_error, cv_max_error]).transpose(),
+                fmt='%.3g', # low resolution is good enough for error data.
+                )
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.loglog(myt,cv_error)
+
+plt.figure()
+plt.plot(wideT,heat_capacity(wideT, Ebest, Sbest, N))
+plt.show()
