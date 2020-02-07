094b468a4eea7bf7c04cf18736810eaec4af894c
Jordan-Pommerenck <jordan.pommerenck@gmail.com>
2020-02-07 13:59:52 -0800

    script now adds heat capacity for plotting.

--- scripts-python-make-data/ising-multi-heat-capacity.py
+++ scripts-python-make-data/ising-multi-heat-capacity.py
@@ -16,6 +16,17 @@ parser = argparse.ArgumentParser(description =
         This script functions by reading in yaml files from a given directory
         and comparing with a given reference .DAT file. The heat capacity
         is computed and compared to the reference.
+        
+        EXAMPLE USAGE:
+        python3 scripts-python-make-data/ising-multi-heat-capacity.py
+        --file_dir=../ising-cp-data/
+        --reference=../deft/papers/histogram/data/ising-32-reference-lndos.dat
+        --save_dir=ising/data/comparison/N32
+        --filename ising-sad-32
+        --N=32
+        --Emin=2048
+        --Emax=0
+        --seed_avg=8
     """)
 
 parser.add_argument('--file_dir', required=True,
@@ -27,27 +38,36 @@ parser.add_argument('--reference', required=True,
 parser.add_argument('--save_dir', required=True,
                     help='The directory where the data will be saved. \
                           Exmaple:data/comparison/N32')
-parser.add_argument('--N', type=int,
+parser.add_argument('--N', type=int, required=True,
                     help='The length of the ising model. Used to divide \
                           the number of moves.')
-parser.add_argument('--Emin', type=int,
+parser.add_argument('--Emin', type=int, required=True,
                     help='The energy at the entropy minimum.')
-parser.add_argument('--Emax', type=int,
+parser.add_argument('--Emax', type=int, required=True,
                     help='The energy at the entropy maximum.')
-
+parser.add_argument('--seed_avg', type=int, required=True,
+                    help='The number of seed files to average over.')
+parser.add_argument('--filename', required=True, nargs='+',
+                    help='The filename to perform analysis on. \
+                          Exmaple:ising-sad-32')
 
 args = parser.parse_args()
 
-file_dir = args.file_dir                # change name from filename_location
-ref = args.reference                    # reference to ref
-save_dir = args.save_dir                # filebase
+# Rename all argparse parameters.
+file_dir = args.file_dir
+ref = args.reference
+save_dir = args.save_dir
+N = args.N
+Emin = args.Emin
+Emax = args.Emax
+seed_avg = args.seed_avg
 
-
-
-# seed_avg = int(sys.argv[8])
-
-# filename = sys.argv[9:]
-# print(('filenames are ', filename))
+# The correct way to handle accepting multiple arguments using nargs
+# to create a list.
+# '+' == 1 or more.
+# '*' == 0 or more.
+# '?' == 0 or 1.
+filename = args.filename
 
 def heat_capacity(T, E, S):
     """
@@ -65,126 +85,139 @@ def heat_capacity(T, E, S):
         C[i] = ((E-U)**2*P).sum()/T[i]**2
     return C
 
-#T = np.arange(0.01, 0.05000001, 0.05000001*0.01)
 T = np.arange(0.01, 5, 0.001)
 
-
-#n_energies = int(Emin - Emax+1)
-
+# Compute the reference energy, entropy, and heat capacity once.
 try:
     eref, lndosref, Nrt_ref = readnew.e_lndos_ps(ref)
 except:
     eref, lndosref = readnew.e_lndos(ref)
 
-CV_ref = heat_capacity(T, eref, lndosref)
+#cvref = heat_capacity(T, eref, lndosref)
+cvref = heat_capacity(T, eref[0:Emin-Emax+1], lndosref[0:Emin-Emax+1])
 
-plt.plot(T,CV_ref)
-plt.show()
+for f in filename:
+    err_in_S = []
+    err_max = []
+    min_moves = []
+    
+    err_in_cv = []
+    name = '%s.yaml' % (f)
+    for n in range(1, seed_avg+1):
+        #try:
+            name = '%s-s%s.yaml' % (f, n)
+            print(('trying filename ', name))
 
-# for f in filename:
-    # err_in_S = []
-    # err_max = []
-    # min_moves = []
-    # name = '%s.yaml' % (f)
-    # for n in range(1, seed_avg+1):
-        # #try:
-            # name = '%s-s%s.yaml' % (f, n)
-            # print(('trying filename ', name))
+            # Read YAML file
+            if os.path.isfile(file_dir + name):
+                with open(file_dir + name, 'r') as stream:
+                    yaml_data = yaml.load(stream)
+            else:
+                print(('unable to read file', file_dir + name))
+                raise ValueError("%s isn't a file!" % (file_dir + name))
 
-            # # Read YAML file
-            # if os.path.isfile(filename_location + name):
-                # with open(filename_location + name, 'r') as stream:
-                    # yaml_data = yaml.load(stream)
-            # else:
-                # print(('unable to read file', filename_location + name))
-                # raise ValueError("%s isn't a file!" % (filename_location + name))
+            data = yaml_data
+            data['bins']['histogram'] = np.array(data['bins']['histogram'])
+            data['bins']['lnw'] = np.array(data['bins']['lnw'])
 
-            # data = yaml_data
-            # data['bins']['histogram'] = np.array(data['bins']['histogram'])
-            # data['bins']['lnw'] = np.array(data['bins']['lnw'])
+            data['movies']['entropy'] = np.array(data['movies']['entropy'])
+            lndos = data['movies']['entropy']
+            energies = data['movies']['energy']
+            N_save_times = len(data['movies']['entropy'])
+            try:
+                maxyaml = energies.index(-Emin)
+            except:
+                my_minE = energies[0]
+                num_new_energies = int(my_minE - (-Emin))
+                print(('num_new_maxyaml', num_new_energies))
+                lndos_new = np.zeros((lndos.shape[0], lndos.shape[1]+num_new_energies))
+                lndos_new[:, num_new_energies:] = lndos[:,:]
+                lndos = lndos_new
+                energies = [0]*num_new_energies + energies
+                maxyaml = 0
 
-            # data['movies']['entropy'] = np.array(data['movies']['entropy'])
-            # lndos = data['movies']['entropy']
-            # energies = data['movies']['energy']
-            # N_save_times = len(data['movies']['entropy'])
-            # try:
-                # maxyaml = energies.index(-Emin)
-            # except:
-                # my_minE = energies[0]
-                # num_new_energies = int(my_minE - (-Emin))
-                # print(('num_new_maxyaml', num_new_energies))
-                # lndos_new = np.zeros((lndos.shape[0], lndos.shape[1]+num_new_energies))
-                # lndos_new[:, num_new_energies:] = lndos[:,:]
-                # lndos = lndos_new
-                # energies = [0]*num_new_energies + energies
-                # maxyaml = 0
+            try:
+                minyaml = energies.index(-Emax)
+            except:
+                my_maxE = energies[-1]
+                num_new_energies = -int(my_maxE - (-Emax))
+                print(('num_new_minyaml', num_new_energies))
+                lndos_new = np.zeros((lndos.shape[0], lndos.shape[1]+num_new_energies))
+                lndos_new[:, :lndos.shape[1]] = lndos[:,:]
+                lndos = lndos_new
+                minyaml = lndos.shape[1]-1
 
-            # try:
-                # minyaml = energies.index(-Emax)
-            # except:
-                # my_maxE = energies[-1]
-                # num_new_energies = -int(my_maxE - (-Emax))
-                # print(('num_new_minyaml', num_new_energies))
-                # lndos_new = np.zeros((lndos.shape[0], lndos.shape[1]+num_new_energies))
-                # lndos_new[:, :lndos.shape[1]] = lndos[:,:]
-                # lndos = lndos_new
-                # minyaml = lndos.shape[1]-1
+            errorinentropy = np.zeros(N_save_times)
+            maxerror = np.zeros(N_save_times)
+            cverror = np.zeros(N_save_times)
 
-            # try:
-                # eref, lndosref, Nrt_ref = readnew.e_lndos_ps(ref)
-            # except:
-                # eref, lndosref = readnew.e_lndos(ref)
+            for i in range(0, N_save_times):
+                # below just set average S equal between lndos and lndosref
+                if 'ising' in save_dir:
 
-            # errorinentropy = np.zeros(N_save_times)
-            # maxerror = np.zeros(N_save_times)
+                    ising_norm = lndos[i][maxyaml:minyaml+1] # remove impossible state
 
-            # for i in range(0, N_save_times):
-                # # below just set average S equal between lndos and lndosref
-                # if TRUE:
-                    # # if using yaml as a reference the range is from 0 to len while for C++ the range is
-                    # # from maxref to minref + 1
-                    # if 'ising' in filebase:
+                    ising_lndos = lndos[i][maxyaml:minyaml+1][::-1] # remove impossible state
 
-                        # ising_norm = lndos[i][maxyaml:minyaml+1] # remove impossible state
+                    # the states are counted backward hence the second to last state would be at index = 1
+                    ising_norm = np.delete(ising_norm, [1])
+                    ising_lndos = np.delete(ising_lndos, [len(ising_lndos)-2])
 
-                        # ising_lndos = lndos[i][maxyaml:minyaml+1][::-1] # remove impossible state
+                    ising_E = np.array(energies[maxyaml:minyaml+1])
+                    ising_E = np.delete(ising_E, [len(ising_E)-2])
+                    
+                    # invoke np.flip since ising_E is indexed backward!
+                    # this is critical for my_cv_error or you get wrong answer.
+                    flip_ising_E = np.flip(np.copy(ising_E))
 
-                        # # the states are counted backward hence the second to last state would be at index = 1
-                        # ising_norm = np.delete(ising_norm, [1])
-                        # ising_lndos = np.delete(ising_lndos, [len(ising_lndos)-2])
+                    norm_factor = np.mean(ising_norm) - np.mean(lndosref[0:Emin-Emax+1])
+                    doserror = ising_lndos - lndosref[0:Emin-Emax+1] - norm_factor
 
-                        # ising_E = np.array(energies[maxyaml:minyaml+1])
-                        # ising_E = np.delete(ising_E, [len(ising_E)-2])
+                    my_cv_error = heat_capacity(T, flip_ising_E, ising_lndos) - cvref
 
+                else:
+                    print('Error! ising must be in save_dir pathname.')
 
-                        # norm_factor = np.mean(ising_norm) - np.mean(lndosref[0:minref-maxref+1])
-                        # doserror = ising_lndos - lndosref[0:minref-maxref+1] - norm_factor
-                    # else:
-                        # norm_factor = np.mean(lndos[i][maxyaml:minyaml+1]) - np.mean(lndosref[0:minref-maxref+1])
-                        # doserror = lndos[i][maxyaml:minyaml+1][::-1] - lndosref[0:minref-maxref+1] - norm_factor
-                # else:
-                    # norm_factor = np.mean(lndos[i][maxyaml:minyaml+1]) - np.mean(lndosref[maxref:minref+1])
-                    # doserror = lndos[i][maxyaml:minyaml+1][::-1] - lndosref[maxref:minref+1] - norm_factor
-
-                # errorinentropy[i] = np.sum(abs(doserror))/len(doserror) #- np.mean(doserror)
-                # maxerror[i] = np.amax(doserror) - np.amin(doserror)
-
-            # # remove N from moves in yaml file because N is added back in the
-            # # comparison-plot script
-            # moves = data['movies']['time']
-            # if min_moves == [] or len(min_moves) > len(moves):
-                # min_moves = np.array(moves)/N
-            # errorinentropy = errorinentropy[:len(moves)]
-            # maxerror = maxerror[:len(moves)]
-            # err_in_S.append(errorinentropy)
-            # err_max.append(maxerror)
+                errorinentropy[i] = np.sum(abs(doserror))/len(doserror) #- np.mean(doserror)
+                maxerror[i] = np.amax(doserror) - np.amin(doserror)
+                cverror[i] = np.sum(abs(my_cv_error))/len(my_cv_error)
+            
+            # remove N from moves in yaml file because N is added back in the
+            # comparison-plot script
+            moves = data['movies']['time']
+            if len(min_moves) == 0 or len(min_moves) > len(moves):
+                min_moves = np.array(moves)/N
+            errorinentropy = errorinentropy[:len(moves)]
+            maxerror = maxerror[:len(moves)]
+            err_in_S.append(errorinentropy)
+            err_max.append(maxerror)
+            
+            err_in_cv.append(cverror)
 
 
-    # for i in range(len(err_in_S)):
-        # err_in_S[i] = err_in_S[i][:len(min_moves)]
-        # err_max[i] = err_max[i][:len(min_moves)]
-    # errorinentropy = np.average(err_in_S, axis=0)
-    # maxmean = np.amax(err_in_S, axis=0)
-    # minmean = np.amin(err_in_S, axis=0)
-    # maxerror = np.average(err_max, axis=0)
-    # dirname = 'data/comparison/%s-%s' % (filebase, name.replace('-s%s.yaml' %seed_avg, ''))
+    for i in range(len(err_in_S)):
+        err_in_S[i] = err_in_S[i][:len(min_moves)]
+        err_max[i] = err_max[i][:len(min_moves)]
+        
+        err_in_cv[i] = err_in_cv[i][:len(min_moves)]
+
+    errorinentropy = np.average(err_in_S, axis=0)
+    maxmean = np.amax(err_in_S, axis=0)
+    minmean = np.amin(err_in_S, axis=0)
+    maxerror = np.average(err_max, axis=0)
+    
+    cverror = np.average(err_in_cv, axis = 0)
+    dirname = '%s-%s' % (save_dir, name.replace('-s%s.yaml' %seed_avg, ''))
+    
+    print('saving to', dirname)
+    try:
+        os.mkdir(dirname)
+    except OSError:
+        pass
+    else:
+        print("Successfully created the directory %s " % dirname)
+    np.savetxt('%s/errors.txt' %(dirname),
+              np.c_[min_moves, errorinentropy, maxerror, minmean, maxmean, cverror],
+              fmt = ('%.4g'),
+              delimiter = '\t',
+              header = 'iterations\t errorinentropy\t maxerror\t minmean\t maxmean\t cverror\t (generated with python %s' % ' '.join(sys.argv))
