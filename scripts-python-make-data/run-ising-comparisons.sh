b8d7de7ec8dfb632ad53842c5d992a1d02548965
Jordan-Pommerenck <jordan.pommerenck@gmail.com>
2020-04-08 15:41:32 -0700

    change script slightly.

--- scripts-python-make-data/run-ising-comparisons.sh
+++ scripts-python-make-data/run-ising-comparisons.sh
@@ -25,5 +25,3 @@ python figs/yaml-multi-comparison.py /home/jordan/ising-cp-data/ /home/jordan/de
 python3 scripts-python-make-data/ising-multi-heat-capacity.py --file_dir=../ising-cp-data/ --reference=../deft/papers/histogram/data/ising-32-reference-lndos.dat --save_dir=ising/data/comparison/N32 --filename ising-sad-32 ising-samc-1e5-32 ising-samc-1e6-32 ising-samc-1e7-32 ising-wl-32 ising-wl-inv-t-32 --N=32 --Emin=2048 --Emax=0 --seed_avg=8
 
 python3 scripts-python-make-data/ising-multi-heat-capacity.py --file_dir=../sad-monte-carlo/ --reference=../deft/papers/histogram/data/ising-32-reference-lndos.dat --save_dir=ising/data/comparison/N32 --filename ising-wl-32-minGamma --N=32 --Emin=2048 --Emax=0 --seed_avg=8
-
-python3 scripts-python-make-data/ising-multi-heat-capacity.py --file_dir=../sad-monte-carlo/ --reference=../deft/papers/histogram/data/ising-32-reference-lndos.dat --save_dir=ising/data/comparison/N32 --filename ising-wl-32-minGamma --N=32 --Emin=2048 --Emax=0 --seed_avg=1
