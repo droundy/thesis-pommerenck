be7f4468c412d4408c7e045358b710824b957c8c
Jordan-Pommerenck <jordan.pommerenck@gmail.com>
2020-04-08 15:37:47 -0700

    generate more data and make the script consider a set temp range.

--- scripts-python-make-data/ising-multi-heat-capacity.py
+++ scripts-python-make-data/ising-multi-heat-capacity.py
@@ -85,7 +85,7 @@ def heat_capacity(T, E, S):
         C[i] = ((E-U)**2*P).sum()/T[i]**2
     return C
 
-T = np.arange(0.01, 5, 0.001)
+T = np.arange(1.5, 5, 0.01)
 
 # Compute the reference energy, entropy, and heat capacity once.
 try:
