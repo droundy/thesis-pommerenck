b44127fab213a3cf6f41a01b69ed03a9b4521fe3
Jordan-Pommerenck <jordan.pommerenck@gmail.com>
2020-04-21 15:35:46 -0700

    made some paper changes.

--- scripts-python-make-data/run-ising-comparisons.sh
+++ scripts-python-make-data/run-ising-comparisons.sh
@@ -17,6 +17,9 @@ python figs/parse-ising-out.py /home/jordan/ising-cp-data/ising-wl-inv-t-128-s1 
 python figs/parse-ising-out.py /home/jordan/sad-monte-carlo/ising-wl-128-minGamma-s1 128 ising-wl-128-minGamma
 #cp data/gamma/ising/n128/ising-wl-128-minGamma.txt ../../../thesis-pommerenck/ising/data/gamma/n128/.
 
+python figs/parse-ising-out.py /home/jordan/sad-monte-carlo/ising-wl-128-minGamma-1e6-s1 128 ising-wl-128-minGamma-1e6
+#cp data/gamma/ising/n128/ising-wl-128-minGamma-1e6.txt ../../../thesis-pommerenck/ising/data/gamma/n128/.
+
 # This is the reference for 32 X 32 system
 #python yaml-multi-comparison.py /home/jordan/ising-cp-data/ /home/jordan/deft/papers/histogram/data/ising-32-reference-lndos.dat ising/N32 32 2048 0 true 8 ising-sad-32 ising-samc-1e5-32 ising-samc-1e6-32 ising-samc-1e7-32 ising-wl-32 ising-wl-inv-t-32
 # python3 scripts-python-make-data/ising-multi-heat-capacity.py --file_dir=../ising-cp-data/ --reference=../deft/papers/histogram/data/ising-32-reference-lndos.dat --save_dir=ising/data/comparison/N32 --N=32 --Emin=2048 --Emax=0 --seed_avg=8 --filename ising-sad-32 ising-samc-1e5-32 ising-samc-1e6-32 ising-samc-1e7-32 ising-wl-32 ising-wl-inv-t-32 
