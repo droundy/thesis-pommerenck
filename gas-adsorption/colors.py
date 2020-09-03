2e0431be5dda4e42fa8096aedd510370efbf0caa
David Roundy <daveroundy@gmail.com>
2020-09-02 17:19:40 -0700

    use nicer color list

--- gas-adsorption/colors.py
+++ gas-adsorption/colors.py
@@ -6,17 +6,27 @@ from matplotlib.font_manager import FontProperties
 small_font = FontProperties()
 small_font.set_size('small')
 
+kelly_colors = [# 'F2F3F4',
+                '222222', 'F3C300', '875692', 'F38400', 'A1CAF1', 'BE0032', 'C2B280', '848482', '008856', 'E68FAC', '0067A5', 'F99379', '604E97', 'F6A600', 'B3446C', 'DCD300', '882D17', '8DB600', '654522', 'E25822', '2B3D26']
+kelly_colors = ['#'+c for c in kelly_colors]
+
 prop_cycle = plt.rcParams['axes.prop_cycle']
-colors = list(prop_cycle.by_key()['color'])
+print(list(prop_cycle.by_key()['color']))
+print(kelly_colors)
+colors = kelly_colors # list(prop_cycle.by_key()['color'])
 
 the_order = [
+    'MOF519',
+    'NJUBai43',
+    'NU1000',
     'MOF5',
     'HKUST1',
+    'HKUST1-mono',
     'PCN14',
+    'AX21',
+    'Mg2dobdc',
     'Ni2dobdc',
     'Co2dobdc',
-    'Mg2dobdc',
-    'AX21',
     'NU125',
     'NOTT112',
     'rhtMOF7',
@@ -28,14 +38,12 @@ the_order = [
     'CYCU3Al',
     'Zn2bdc2dabco',
     'COF102',
-    'HKUST1-mono',
-    'MOF519',
-    'NJUBai43',
 ]
 
 # cm = plt.get_cmap('viridis')
-for x in np.linspace(0, 1, len(the_order) - len(colors)):
-    colors.append(matplotlib.colors.hsv_to_rgb((x,1,1-x*0.25)))
+if len(colors) < len(the_order):
+    for x in np.linspace(0, 1, len(the_order) - len(colors)):
+        colors.append(matplotlib.colors.hsv_to_rgb((x,1,1-x*0.25)))
 # colors = [cm(1.*i/(len(the_order)-1)) for i in range(len(the_order))]
 
 def order(mofs):
